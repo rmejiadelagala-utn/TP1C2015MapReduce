d501098363109a76e42706997b33250aef34f088
logonzalez <loreglez.utn@gmail.com>
2015-04-29 00:08:49 -0300

    Modificación proceso Nodo

--- Nodo/nodo.c
+++ Nodo/nodo.c
@@ -5,8 +5,30 @@
  *      Author: utnso
  */
 #include<stdio.h>
-int main(){
-	printf("Hola soy nodo\n");
-}
+#include<commons/config.h>
+#include<socketes/servidor.h>
 
+int main() {
+	printf("Hola soy proceso Nodo\n");
+
+	char* path = "/home/utnso/ConfigNodo.txt";
+
+	t_config* config;
+	config = config_create(path);
+
+	printf("Conectando a IP: %s\n", config_get_string_value(config, "IP_FS"));
+	printf("Puerto: %d\n", config_get_int_value(config, "PUERTO_FS"));
+	printf("Archivo_bin: %s\n", config_get_string_value(config, "ARCHIVO_BIN"));
+	printf("Directorio Temporal: %s\n", config_get_string_value(config, "DIR_TEMP"));
+	printf("Nodo Nuevo: %s\n", config_get_string_value(config, "NODO_NUEVO"));
+	printf("IP Nodo: %s\n", config_get_string_value(config, "IP_NODO"));
+	printf("Puerto Nodo: %d\n", config_get_int_value(config, "PUERTO_NODO"));
+
+	int sockfs;
+	sockfs = crearCliente(config_get_string_value(config,"IP_FS"),config_get_int_value(config, "PUERTO_FS"));
+
+
+	return 0;
+
+}
 
