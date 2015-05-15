06f77b17086edbd6b8e595be8e84c2edae384301
rmejiadelagala-utn <rmejiadelagala-utn@users.noreply.github.com>
2015-05-15 19:07:43 -0300

    agrego la funcion leer_archivo_configuración
    la funcion leer_archivo_configuracion recibe como parametro el puntero al archivo de configuracion, y devolverá un puntero a estructura que se carga con todo lo que se lleyó del archivo de configuración. De esta struct tomaremos los datos para armar la struct que necesitamos enviar a marta serializada.

--- Job/job.c
+++ Job/job.c
@@ -9,23 +9,22 @@
 #include<string.h>
 #include<commons/config.h>
 #include<socketes/servidor.h>
+#include"job.h"
 
 int main(int argc,char *argv[]) {
 
-	t_config *config;
 	char *c;
 	int sock;
+	t_config_job* info_config;
+	info_config=(t_config_job*)malloc(sizeof(t_config_job));
 	if(argc!=2){
 		printf("Error: Uso: ./job path_archconfig\n");
 		return 1;
 	}
 	c=argv[1];
-	printf("recibo este archivo de configuracion: ");
-	printf(" %s\n",c);
-	config = config_create(c);
-	printf("conectando a IP:  %s\n",config_get_string_value(config,"IP"));
-	printf("en el Puerto: %d\n",config_get_int_value(config, "PORT"));
-	sock= crearCliente(config_get_string_value(config,"IP"),config_get_int_value(config, "PORT"));
+	info_config=leer_archivo_configuracion(c);
+//	Por ahora comento lo del socket, ya que voy a usar el puntero a t_config_job	
+//	sock= crearCliente(config_get_string_value(config,"IP"),config_get_int_value(config, "PORT"));
 	return 0;
 }
 
