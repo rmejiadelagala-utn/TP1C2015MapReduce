2a30c03b925dd6d53029eec74e403f67c1367658
JuanchiRios <juanchirios04@hotmail.com>
2015-05-31 14:27:43 -0300

    Agrego FuncionesFilesSystem source y header. Para meter las funciones core del FS, como distribuirBloquesEnNodos

---
+++ Filesystem/funcionesFileSystem.h
@@ -0,0 +1,18 @@
+/*
+ * funcionesFileSystem.h
+ *
+ *  Created on: 31/5/2015
+ *      Author: JuanRios
+ */
+
+#ifndef FUNCIONESFILESYSTEM_H_
+#define FUNCIONESFILESYSTEM_H_
+
+/*
+  	* @NAME: distribuirBloquesEnNodos
+  	* @DESC: recibe una lista de bloques de archivo, por la cual crea 3 copias y cada una se la asigna a un nodo específico. Ademas internamente ordena la lista para lograr una mejor distribucion
+  	*/
+void distribuirBloquesEnNodos(t_list *bloquesEnArch, t_list *nodos);
+
+
+#endif /* FUNCIONESFILESYSTEM_H_ */
