b013ac5ad40cb790a00696682f1b9d9e7771dc10
rmejiadelagala-utn <rmejiadelagala.utn@gmail.com>
2015-06-04 23:45:23 -0300

    creo funcionesJob para modularizar

--- Job/job.h
+++ Job/job.h
@@ -11,6 +11,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<commons/config.h>
+#include<socketes/servidor.h>
 
 #define TRUE 1
 #define FALSE 0
@@ -41,54 +43,6 @@ typedef struct {
 	char*  archivo_resultado;
 } t_config_job;
 
-t_config_job* leer_archivo_configuracion(char* arch){
-	t_config *config;
-	t_config_job* info_config;
-	info_config=(t_config_job*)malloc(sizeof(t_config_job));
-	printf("recibo este archivo de configuracion: ");
-	printf(" %s\n",arch);
-	config = config_create(arch);
-	info_config->IP=strdup(config_get_string_value(config,"IP"));
-	printf("conectando a IP:  %s\n",info_config->IP);
-	info_config->PORT= config_get_int_value(config, "PORT");
-	printf("en el Puerto: %d\n",info_config->PORT);
-	info_config->MAPPER=strdup(config_get_string_value(config,"MAPPER"));
-	printf("Este es el mapper:  %s\n",info_config->MAPPER);
-	info_config->REDUCE=config_get_string_value(config,"REDUCE");
-	printf("Este es el reduce:  %s\n",info_config->REDUCE);
-	info_config->COMBINER=strdup(config_get_string_value(config,"COMBINER"));
-	printf("Soprta Combiner?:  %s\n",info_config->COMBINER);
-	info_config->archivo_resultado=strdup(config_get_string_value(config,"RESULTADO"));
-	printf("Este es el archivo resultado:  %s\n",info_config->archivo_resultado);
-//	char** archivos = config_get_array_value(config,"ARCHIVOS");
-	info_config->archivos=config_get_array_value(config,"ARCHIVOS");
-	int i=0;
-	while(info_config->archivos[i]!='\0'){
-//	   info_config->archivos[i]=strdup(archivos[i]);
-	   printf("archivo[%d]=  %s\n",i,info_config->archivos[i]);
-	   i++;
-	}
 
-	return info_config;
-}
-
-uint32_t cantidadArchivosToProcesar(t_solicitud* sol){
-	uint32_t i=0;
-	while(sol->archivos[i] != 0){
-		i++;
-	}
-	return i;
-}
-
-uint32_t tamanioBufferSerializar(t_solicitud* sol) {
-	int i=0;
-	size_t tam = strlen(sol->archivo_resultado);
-	tam += sizeof(sol->combiner);
-	while(sol->archivos[i] != 0){
-		tam+= strlen(sol->archivos[i]);
-		i++;
-	}
-	return (uint32_t)tam++;
-}
 #endif /* JOB_H_ */
 
