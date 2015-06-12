815f9daa8a2bf7a5ef284af62920e44e052e83e0
rmejiadelagala-utn <rmejiadelagala.utn@gmail.com>
2015-06-11 21:52:34 -0300

    agrego los strdup para copiar los string del archivo de configuracion

--- Nodo/funcionesNodo.c
+++ Nodo/funcionesNodo.c
@@ -22,12 +22,12 @@ t_config_nodo* leerArchivoConfig(char *path_config){
 		exit(-1);
 	}
 
-	configNodo->IP_FS     = config_get_string_value(config, "IP_FS");
+	configNodo->IP_FS     = strdup(config_get_string_value(config, "IP_FS"));
 	configNodo->PUERTO_FS = config_get_int_value(config, "PUERTO_FS");
-	configNodo->ARCH_BIN  = config_get_string_value(config, "ARCHIVO_BIN");
-	configNodo->DIR_TEMP  = config_get_string_value(config, "DIR_TEMP");
-	configNodo->NODO_NEW  = config_get_string_value(config, "NODO_NUEVO");
-	configNodo->IP_NODO   = config_get_string_value(config, "IP_NODO");
+	configNodo->ARCH_BIN  = strdup(config_get_string_value(config, "ARCHIVO_BIN"));
+	configNodo->DIR_TEMP  = strdup(config_get_string_value(config, "DIR_TEMP"));
+	configNodo->NODO_NEW  = strdup(config_get_string_value(config, "NODO_NUEVO"));
+	configNodo->IP_NODO   = strdup(config_get_string_value(config, "IP_NODO"));
 	configNodo->PUERTO_NODO = config_get_int_value(config, "PUERTO_NODO");
 
 /*	printf("Conectando a IP: %s\n", config_get_string_value(config, "IP_FS"));
