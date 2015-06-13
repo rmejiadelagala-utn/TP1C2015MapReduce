9a4281c62cded6ddd43ef4752f0e5678a6a90dc7
JuanchiRios <juanchirios04@hotmail.com>
2015-06-12 23:23:28 -0300

    arreglo la var resultado que estaba estallando al compilar

--- Filesystem/funcionesFileSystem.c
+++ Filesystem/funcionesFileSystem.c
@@ -653,7 +653,7 @@ int obtenerArchivo(t_archivo *archivo) {
 	t_stream *stream = empaquetar_mensaje(mensaje);
 	int result = send(nodoEncontrado->socket,stream->length,stream,0);
 	if (result<0) return result;
-	int result = send(nodoEncontrado->socket,stream->data,stream,0);
+	result = send(nodoEncontrado->socket,stream->data,stream,0);
 	if (result<0) return result;
 	sem_wait(&semaforo);
 		//HAY QUE CREAR EL CAMPO SOCKET EN NODO
