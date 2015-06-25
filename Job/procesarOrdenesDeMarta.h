969946dbc49fe03b5556a65342a53958dd2fc1c9
rmejiadelagala-utn <rmejiadelagala.utn@gmail.com>
2015-06-24 22:21:25 -0300

    commit inicial de funcion procesarOrdenesDeMarta

---
+++ Job/procesarOrdenesDeMarta.h
@@ -0,0 +1,15 @@
+/*
+ * procesarOrdenesDeMarta.h
+ *
+ *  Created on: 24/6/2015
+ *      Author: rmejia
+ */
+
+#ifndef PROCESARORDENESDEMARTA_H_
+#define PROCESARORDENESDEMARTA_H_
+
+#include "job.h"
+
+int procesarOrdenesDeMarta(int sockMarta);
+
+#endif /* PROCESARORDENESDEMARTA_H_ */
