281b94d8f67ecc6edcd705bc54c5a0383268c23d
Lorena Gonzalez <loreglez.utn@gmail.com>
2015-06-25 07:52:39 -0300

    Agrego función para crear script de Reduce

--- Nodo/funcionesNodo.c
+++ Nodo/funcionesNodo.c
@@ -171,3 +171,37 @@ void crearScriptMapper(const char* codigo_script){
 
 	return;
 }
+
+void crearScriptReduce(const char* codigo_script){
+
+	FILE* fd;
+	FILE* scriptReduce;
+	char texto[100];
+
+//	if((fd =fopen("/home/utnso/Escritorio/TPSO-2015/reduce.pl","r"))==NULL){
+//		perror("Error al abrir el script del reduce");
+//		exit(1);
+//	}
+
+//	while (feof(fd) == 0) {
+//		fgets(texto,100,fd);
+//		printf( "%s",texto );
+//	}
+
+
+	if((scriptReduce=fopen("/home/utnso/reduce.pl","w+"))==NULL){
+		perror("Error al crear el script del mapper");
+		exit(1);
+	}
+
+//	while (feof(fd) == 0) {
+//		fgets(texto,100,fd);
+//		fputs(texto,scriptReduce);
+//	}
+
+	fputs(codigo_script,scriptReduce);
+	fclose(scriptReduce);
+
+	return;
+
+}
