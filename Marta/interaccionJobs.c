d096ed3bc7d725586bf4dd3e67877d5435a668af
j9794 <j9794>
2015-07-21 12:54:20 -0300

    Empece a agregar retornos en las recepciones de mensajes para detectar errores.

--- Marta/interaccionJobs.c
+++ Marta/interaccionJobs.c
@@ -9,6 +9,13 @@ void *interaccionJobs(void* sock_ptr) {
 
 	t_solicitud solicitud = deserealizarSolicitudDeJob(sockCliente);
 
+	if(!solicitud){
+		log_error(marta_logger,"Se produjo un error con el job");
+		close(sockCliente);
+		pthread_mutex_unlock(&conexionFS);
+		return NULL;
+	}
+
 	t_InfoJob info_job = adaptarSolicitudAInfoJob(solicitud);
 
 	t_list* listaDeArchivos = obtenerIDyCantBloquesDeArchivosDelFS(info_job.pathsDeArchivos,
@@ -52,10 +59,11 @@ void *interaccionJobs(void* sock_ptr) {
 
 
 t_solicitud deserealizarSolicitudDeJob(int sockCliente){
-	int i;
+		int i;
 
 		t_solicitud solicitud;
-		solicitud.cantArchivos = recibirInt(sockCliente);
+
+		if((solicitud.cantArchivos = recibirInt(sockCliente))<1)return NULL;
 
 		log_info(marta_logger,"La cantidad de archivos recibidos es de %d",solicitud.cantArchivos);
 
@@ -63,12 +71,12 @@ t_solicitud deserealizarSolicitudDeJob(int sockCliente){
 
 		for (i = 0; i < solicitud.cantArchivos; i++) {
 
-			solicitud.archivos[i] = recibirString(sockCliente);
+			if((solicitud.archivos[i] = recibirString(sockCliente))<1) return NULL;
 
 		}
 
-		solicitud.archivo_resultado=recibirString(sockCliente);
-		solicitud.combiner = recibirInt(sockCliente);
+		if((solicitud.archivo_resultado=recibirString(sockCliente))<1) return NULL;
+		if((solicitud.combiner = recibirInt(sockCliente))<1) return NULL;
 
 		log_info(marta_logger,"Mostrare los datos del paquete deserealizados\n");
 		log_info(marta_logger,"Combiner: %d",solicitud.combiner);
